8757b18b99b9829069d88187d4ceea681fea1f93
vcrl1 <vcrl1994@gmail.com>
2023-03-29 09:44:23 +0200

    Pocos cambios adicionales.

--- src/com/adecco/presentacion/Main.java
+++ src/com/adecco/presentacion/Main.java
@@ -2,6 +2,7 @@ package com.adecco.presentacion;
 
 import com.adecco.modelo.Agua;
 import com.adecco.modelo.Bebida;
+import com.adecco.modelo.Gaseosa;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,7 +10,14 @@ public class Main {
         System.out.println(a1);
         Agua a2= (Agua) a1;
         a2.setOrigen("Sierra Nevada");
-        System.out.println(a2.getOrigen());
+        System.out.println(a2);
+
+        Bebida g1=new Gaseosa("Refresco","Coca-cola", 1, 0.005);
+//pasa tiempo y quiero modificar un dato.
+        Gaseosa g2= (Gaseosa) g1;
+        g2.setCantidadAzucar(0.5);
+        System.out.println(g1);
+        System.out.println(g2); //sale lo mismo que el otro porque se ha hecho casting
 
     }
 }
